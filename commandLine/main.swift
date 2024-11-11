ede9952af687af14fe5fddeb42d52ed7dccd9529
DanielComputerScience <elijah.williams@student.allenisd.org>
2024-11-11 09:29:01 -0600

    adding date checker to command line code

--- commandLine/main.swift
+++ commandLine/main.swift
@@ -8,18 +8,36 @@ func getInput(want: String, Question: String) -> String {
 }
 
 func main() {
-    let months = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-    var month = getInput(want: "month", Question: "What month is it?")
-    if let holder = Int(month){
-        let monthValue = holder - 1
-        let convertedMonth = months[monthValue]
-        month = String(convertedMonth)
+    var correct = true
+    repeat {
+        let months = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+        var month = getInput(want: "month", Question: "What month is it?")
+        if let holder = Int(month){
+            let monthValue = holder - 1
+            let convertedMonth = months[monthValue]
+            month = String(convertedMonth)
+        }
+        // Asking user for input day and year
+        let day = getInput(want: "day", Question: "What day is it?")
+        let year = getInput(want: "year", Question: "What year is it?")
+        // Returning entered day
+        print("It is " + month + " " + day + " " + year)
+        // Ask user if the date entered is correct
+        print("Is this date correct (y/n)")
+        var ui = readLine()
+        if ui == "n" {
+            correct = false
+        }
+    } while correct == false
+    
+        print("What celestial body do you want the calendar of?")
+    // This next bit is just used for NASA HUNCH Example not actually working
+    
+    let convertTo = readLine()
+    if convertTo == "Moon" {
+        print("Waning Cresent 2024")
+    } else {
+        print("Sol 632")
     }
-    let day = getInput(want: "day", Question: "What day is it?")
-    let year = getInput(want: "year", Question: "What year is it?")
-    print("It is " + month + " " + day + " " + year)
-    print("What celestial body do you want the calendar of?")
-    readLine()
-    print("Waning Cresent 2024")
 }
 main()
