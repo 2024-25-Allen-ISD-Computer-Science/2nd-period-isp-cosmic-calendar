cea3dede73570e478d1d48a0e69890dedb062883
Lance557 <142256558+Lance557@users.noreply.github.com>
2024-11-13 10:13:28 -0600

    Update main.java

--- commandLine/main.java
+++ commandLine/main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
@@ -5,14 +7,18 @@ class Main {
     System.out.println("What month is it?");
 
     String want = myObj.nextLine();  
-    String[] months = {"January", "February",  "March","April","May","June","July","August","September","October","November","December"};
+    String[] months = {"January", "February", "March","April","May","June","July","August","September","October","November","December"};
+  if(Arrays.asList(months).contains(want)){
      Scanner myObj2 = new Scanner (System.in);
      System.out.println("What day is it?");
-     String want2 = myObj2.nextLine();
+     int want2 = myObj2.nextInt();
      Scanner myObj3 = new Scanner (System.in);
      System.out.println("What year is it?");
-     String want3 = myObj3.nextLine();
+     int want3 = myObj3.nextInt();
      System.out.println("The date is: " + want + " "+ want2+ " "+want3);
-     
-}
+  }
+ 
+  else {
+      System.out.println("Not a valid month");
+  }
 }
