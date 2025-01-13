f742d563c74444a8d58083e7da11adec5660123a
Daniel <110997999+DanielComputerScience@users.noreply.github.com>
2025-01-13 16:26:36 +0000

    Updating js to get info for main calendar

--- home.js
+++ home.js
@@ -1,8 +1,11 @@
 // Get the current date
 var currentDate = new Date();
+var month = currentDate.toLocaleString('default', { month: 'long' });
+var year = currentDate.getFullYear();
 
 // Format the date as desired
-var formattedDate = currentDate.toDateString();
+
 
 // Display the date on the webpage
-document.getElementById('date').innerText = formattedDate;
+document.getElementById('month').innerText = month; 
+document.getElementById('year').innerText = year;
