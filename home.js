85ad457750ca9c6be986ec57603cc052583c9761
Daniel <110997999+DanielComputerScience@users.noreply.github.com>
2025-01-13 16:45:41 +0000

    Finishing Calendar to show current Date

--- home.js
+++ home.js
@@ -1,7 +1,9 @@
 // Get the current date
 var currentDate = new Date();
+// Getting date indexes
 var month = currentDate.toLocaleString('default', { month: 'long' });
 var year = currentDate.getFullYear();
+var day = currentDate.getDate();
 
 // Format the date as desired
 
@@ -9,3 +11,11 @@ var year = currentDate.getFullYear();
 // Display the date on the webpage
 document.getElementById('month').innerText = month; 
 document.getElementById('year').innerText = year;
+// highlight the day
+var days = document.getElementsByClassName('days')[0].getElementsByTagName('li');
+for (var i = 0; i < days.length; i++) {
+    if (days[i].innerText == day) {
+        days[i].classList.add('active');
+    
+    }
+}
