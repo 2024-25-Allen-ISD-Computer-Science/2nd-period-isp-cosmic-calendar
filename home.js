e4276a72f057e93dcdfb157b915d97fc2f10d67a
Daniel <110997999+DanielComputerScience@users.noreply.github.com>
2025-01-13 16:17:01 +0000

    Javascript for date

---
+++ home.js
@@ -0,0 +1,8 @@
+// Get the current date
+var currentDate = new Date();
+
+// Format the date as desired
+var formattedDate = currentDate.toDateString();
+
+// Display the date on the webpage
+document.getElementById('date').innerText = formattedDate;
